153267fe55211e78f4783fd07d520fcc6e84563f
Rafael Veneroso Morici <rvenerosomorici@gmail.com>
2026-04-25 23:18:03 -0300

    Substituir security headers inline por RVM.Common.Security (TASK-217)

--- src/RVM.LiveBoard.API/Program.cs
+++ src/RVM.LiveBoard.API/Program.cs
@@ -8,6 +8,7 @@ using RVM.LiveBoard.API.Middleware;
 using RVM.LiveBoard.API.Services;
 using RVM.LiveBoard.Infrastructure;
 using RVM.LiveBoard.Infrastructure.Data;
+using RVM.Common.Security;
 using Serilog;
 using Serilog.Formatting.Compact;
 
@@ -82,6 +83,7 @@ try
         app.UsePathBase(pathBase);
 
     app.UseForwardedHeaders();
+    app.UseSecurityHeaders();
     app.UseStaticFiles();
     app.UseAntiforgery();
     app.UseMiddleware<CorrelationIdMiddleware>();
